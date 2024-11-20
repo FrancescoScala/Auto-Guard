b3caeb3a1e86767bf18b0729614d6d7e73692620
Zaheer Ahmed <zaheer.kk82@gmail.com>
2024-11-20 17:30:23 +0100

    moved to new repo

--- backend/reports-backend/app.py
+++ backend/reports-backend/app.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, jsonify
 from report import ReportRepo
+import json
 
 app = Flask(__name__)
 
-repo = ReportRepo()
+
 
 @app.route('/echo', methods=['POST'])
 def hello():
+    repo = ReportRepo()
     return jsonify(request.json)
 
 @app.route('/api/reports', methods=['GET'])
 def list_reports():
+    repo = ReportRepo()
     return jsonify(repo.list())
 
 @app.route('/api/reports', methods=['POST'])
 def add_report():
     report = request.json
-    repo.add(report)
+    repo = ReportRepo()
+    repo.add(json.dumps(report))
     return "OK"
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port= 6002, debug=True)
--- backend/reports-backend/report.py
+++ backend/reports-backend/report.py
@@ -1,10 +1,38 @@
-class ReportRepo:
+
+import sqlite3
+from re import finditer
+class ReportRepo(object):
+    def __init__(self):
+        self._db = sqlite3.connect('report_db.sqlite3')
+
+    def __del__(self):
+        self._db.close()
+
+    def __execute(self, stmt, param = None):
+        args = []
+
+
+
+        cursor = self._db.cursor()
+        if param is None:
+            cursor.execute(stmt)
+        else:
+            cursor.execute(stmt, param)
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+
     repo: dict[int, object] = {}
     highest_id = 0
 
     def list(self):
-        return self.repo
+        records = self.__execute("SELECT ID, REPORT_DATE, REPORT_JSON  FROM REPORT")
+        records = [dict(zip(["ID", "REPORT_DATE", "REPORT_JSON"], record)) for record in records]
+        return records
 
-    def add(self, report: object):
-        self.highest_id = self.highest_id + 1
-        self.repo[self.highest_id] = report
+    def add(self, report):
+        records = self.__execute("INSERT INTO REPORT (REPORT_DATE, REPORT_JSON) VALUES (CURRENT_TIMESTAMP, ?)",
+                                 (report,))
+        self._db.commit()
+        return records
