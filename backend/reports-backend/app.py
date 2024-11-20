b412916eceaf9cbd859beff0aa2c4b90390079fb
Giovanni Barbaro <vanni-96@hotmail.it>
2024-11-20 14:19:54 +0000

    Fixes report api

--- backend/reports-backend/app.py
+++ backend/reports-backend/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
-from flask_restful import Api
-from report import ReportsResource
+from flask import Flask, request, jsonify
+from report import ReportRepo
 
 app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(ReportsResource, '/reports')
+repo = ReportRepo()
+
+@app.route('/echo', methods=['POST'])
+def hello():
+    return jsonify(request.json)
+
+@app.route('/api/reports', methods=['GET'])
+def list_reports():
+    return jsonify(repo.list())
+
+@app.route('/api/reports', methods=['POST'])
+def add_report():
+    report = request.json
+    repo.add(report)
+    return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/reports-backend/report.py
+++ backend/reports-backend/report.py
@@ -1,8 +1,3 @@
-import json
-
-from flask import request
-from flask_restful import Resource
-
 class ReportRepo:
     repo: dict[int, object] = {}
     highest_id = 0
@@ -13,15 +8,3 @@ class ReportRepo:
     def add(self, report: object):
         self.highest_id = self.highest_id + 1
         self.repo[self.highest_id] = report
-
-
-class ReportsResource(Resource):
-    report_repo = ReportRepo()
-
-    def get(self):
-        return self.report_repo.list()
-
-    def post(self):
-        report = request.json
-        self.report_repo.add(report)
-        return "OK"
