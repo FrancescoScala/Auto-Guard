eeb7e230b8512fc8177141f46339b288423a5d8b
Giovanni Barbaro <vanni-96@hotmail.it>
2024-11-20 16:47:01 +0100

    Creates single report view

--- backend/reports-backend/app.py
+++ backend/reports-backend/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request, jsonify, render_template
+
 from report import ReportRepo
 
 app = Flask(__name__, static_folder="templates/assets")
 
 repo = ReportRepo()
 
+
 @app.route('/echo', methods=['POST'])
 def hello():
     return jsonify(request.json)
 
+
 @app.route('/api/reports', methods=['GET'])
 def list_reports():
     return jsonify(repo.list())
+
 
 @app.route('/api/reports', methods=['POST'])
 def add_report():
@@ -19,9 +23,17 @@ def add_report():
     repo.add(report)
     return "OK"
 
+
 @app.route("/reports", methods=['GET'])
 def show_reports():
     return render_template('reports.html', reports=repo.list())
+
+
+@app.route("/reports/<int:report_id>", methods=['GET'])
+def show_report(report_id: int):
+    report = repo.list()[report_id]
+    return render_template('report.html', report=report, id=report_id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
